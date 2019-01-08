c8cf52c9051c4d5ae5da203c4cf4545e253a38d2
Jonas Klamroth <jonas.klamroth@gmail.com>
2019-01-08 13:50:57 +0100

    updated TmpTest

--- tests/CaseStudy/TmpTest.java
+++ tests/CaseStudy/TmpTest.java
@@ -11,24 +11,38 @@ public class TmpTest {
     int[] arr;
     TmpTest t2;
     private int privInt;
-    /*
 
+    //@ ensures (\exists int i; i >= 0 && i < 3; (\forall int j; j >= 0 && j < 3; j > i));
     @Fails
-    public void test(int arr[]) {
-        CProver.assume(arr != null);
-        CProver.assume(arr.length == 6);
-        CProver.assume(arr[0] != 0 || arr[0] == 0);
+    private void nestedQuantifierTest() {}
 
-        assert(false);
-    }*/
+    //@ ensures (\forall int i; i >= 0 && i < 3; (\exists int j; j >= 0 && j < 3; j > i));
+    @Fails
+    private void nestedQuantifierTest1() {}
+
+    //@ ensures (\forall int i; i >= 0 && i < 3; (\exists int j; j >= -1 && j < 3; i > j));
+    @Verifyable
+    private void nestedQuantifierTest3() {}
 
     //@ ensures !(\forall int i; i >= 0 && i < 3; (\exists int j; j >= 0 && j < 3; j > i));
     @Verifyable
     private void negatedQuantifierTest() {}
 
-    //@ ensures (\forall int i; i >= 0 && i < 3; (\exists int j; j >= -1 && j < 3; i > j));
+    //@ ensures !(\forall int i; i >= 0 && i < 3; (\exists int j; j >= 0 && j < 3; j > i));
     @Verifyable
-    private void nestedQuantifierTest3() {}
+    private void negatedQuantifierTest1() {}
+
+    //@ ensures !(\forall int i; i >= 0 && i < 3; (\exists int j; j >= -1 && j < 3; i > j));
+    @Fails
+    private void negatedQuantifierTest3() {}
+
+    //@ ensures (\exists int i; i >= 0 && i < 3; !(\forall int j; j >= 0 && j < 2; i > j));
+    @Verifyable
+    private void negatedQuantifierTest4() {}
+
+    //@ ensures (\exists int i; i >= 3 && i < 5; !(\forall int j; j >= 0 && j < 2; i > j));
+    @Fails
+    private void negatedQuantifierTest5() {}
 
     //@ ensures !(\exists int i; i >= 0 && i < 3; (\forall int j; j >= 0 && j < 2; i > j));
     @Fails
@@ -37,7 +51,4 @@ public class TmpTest {
     //@ ensures (\exists int i; i >= 0 && i < 3; (\forall int j; j >= 0 && j < 2; i > j));
     @Verifyable
     private void nestedQuantifierTest2() {}
-
-
-
 }
