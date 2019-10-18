b2fdb8848c18eacafa27be2e97e079db3cf93ec9
Jonas Klamroth <klamroth@fzi.de>
2019-10-18 14:31:51 +0200

    counterexamples now support fields

--- src/XmlParser.java
+++ src/XmlParser.java
@@ -67,7 +67,7 @@ public class XmlParser {
 
     public static JBMCOutput parse(Document xmlDoc, File sourceFile) {
         List<JBMCOutput.Assignment> references = new ArrayList<>();
-        Map<String, String> varNameMap = new HashMap<>();
+        Map<String, String> dynamicObjectsMap = new HashMap<>();
         File f1 = sourceFile;
         List<String> lines = new ArrayList<>();
         try {
@@ -87,7 +87,6 @@ public class XmlParser {
             res.messages.add(messageList.item(i).getTextContent());
         }
         NodeList propertyNodeList = doc.getElementsByTagName("result");
-        boolean someFail = false;
         for(int i = 0; i < propertyNodeList.getLength(); ++i) {
             Node propertyNode = propertyNodeList.item(i);
             if (propertyNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -96,7 +95,6 @@ public class XmlParser {
                 int lineNumber = -1;
                 if(propertyElemnt.getAttribute("status").equals("FAILURE")) {
                     references = new ArrayList<>();
-                    someFail = true;
                     Element failure = (Element)propertyElemnt.getElementsByTagName("failure").item(0);
                     Element location = (Element) failure.getElementsByTagName("location").item(0);
                     lineNumber = Integer.parseInt(location.getAttribute("line"));
@@ -106,45 +104,59 @@ public class XmlParser {
                         if(assignment.getElementsByTagName("location").getLength() > 0) {
                             Element aLocation = (Element) assignment.getElementsByTagName("location").item(0);
                             Element lhs = (Element) assignment.getElementsByTagName("full_lhs").item(0);
-                            Element value = (Element) assignment.getElementsByTagName("value").item(0);
+                            Element value = (Element) assignment.getElementsByTagName("full_lhs_value").item(0);
                             String l = lines.get(Integer.parseInt(aLocation.getAttribute("line")) - 1);
-                            if(assignment.getAttribute("hidden").equals("false") && lhs.getTextContent().contains("this") || lhs.getTextContent().contains("anonlocal") || lhs.getTextContent().contains("arg")) {
+                            if(lhs.getTextContent().contains("dynamic_object") || lhs.getTextContent().contains("tmp_object_factory") || lhs.getTextContent().contains("anonlocal") || lhs.getTextContent().contains("arg")) {
                                 Pattern p = Pattern.compile("(\\w*) ?= ?.*?;");
                                 Matcher m = p.matcher(l);
                                 String guess = null;
-                                if(m.find()) {
-                                    guess = m.group(1);
-                                    references.add(new JBMCOutput.Assignment(aLocation.getAttribute("line"), lhs.getTextContent(), guess, value.getTextContent()));
-                                    if(varNameMap.containsKey(lhs.getTextContent()) && !varNameMap.get(lhs.getTextContent()).equals(guess)) {
-                                        System.out.println("Different guesses for var " + lhs.getTextContent());
-                                    } else {
-                                        varNameMap.put(lhs.getTextContent(), guess);
+                                if(lhs.getTextContent().contains("tmp_object_factory")) {
+                                    guess = lhs.getTextContent().replace("tmp_object_factory.", "");
+                                    //references.add(new JBMCOutput.Assignment(aLocation.getAttribute("line"), lhs.getTextContent(), guess, value.getTextContent()));
+                                    if(value.getTextContent().startsWith("&dynamic_object")) {
+                                        dynamicObjectsMap.put(value.getTextContent().substring(1, value.getTextContent().indexOf('.')), guess);
                                     }
+                                } else if(lhs.getTextContent().startsWith("dynamic_object")) {
+                                    if(lhs.getTextContent().contains(".")) {
+                                        String dynObject = lhs.getTextContent().substring(0, lhs.getTextContent().indexOf('.'));
+                                        if(dynamicObjectsMap.containsKey(dynObject)) {
+                                            guess = lhs.getTextContent().replaceAll("dynamic_object(\\d)*", dynamicObjectsMap.get(dynObject));
+                                        }
+                                    } else {
+                                        //guess = lhs.getTextContent();
+                                        guess = null;
+                                    }
+                                    references.add(new JBMCOutput.Assignment(aLocation.getAttribute("line"), lhs.getTextContent(), guess, value.getTextContent()));
                                 } else {
-                                    if(lhs.getTextContent().contains("arg")) {
-                                        String text = lines.stream().reduce("", String::concat);
-                                        p = Pattern.compile("\\(((\\/\\*@ non_null \\*\\/)?\\s*[\\w\\[\\]]*\\s*(\\w*)\\s*)?(\\s*,\\s*(\\/\\*@ non_null \\*\\/)?\\s*[\\w\\[\\]]*\\s*(\\w*))*\\)\\s*[{]");
-                                        m = p.matcher(text);
-                                        List<String> args = new ArrayList<>();
-                                        int linenNum = Integer.parseInt(aLocation.getAttribute("line"));
-                                        int numChars = lines.subList(0, linenNum).stream().reduce("", String::concat).length();
-                                        while(m.find() && m.start() < numChars) {
-                                            args.clear();
-                                            for(int k = 0; k < m.groupCount(); ++k) {
-                                                if(k % 3 == 2 && m.group(k + 1) != null && !m.group(k + 1).equals("")) {
-                                                    args.add(m.group(k + 1));
-                                                    System.out.println("Found arg " + args.get(args.size() - 1));
+                                    if (m.find()) {
+                                        guess = m.group(1);
+                                        references.add(new JBMCOutput.Assignment(aLocation.getAttribute("line"), lhs.getTextContent(), guess, value.getTextContent()));
+                                    } else {
+                                        if (lhs.getTextContent().contains("arg")) {
+                                            String text = lines.stream().reduce("", String::concat);
+                                            p = Pattern.compile("\\(((\\/\\*@ non_null \\*\\/)?\\s*[\\w\\[\\]]*\\s*(\\w*)\\s*)?(\\s*,\\s*(\\/\\*@ non_null \\*\\/)?\\s*[\\w\\[\\]]*\\s*(\\w*))*\\)\\s*[{]");
+                                            m = p.matcher(text);
+                                            List<String> args = new ArrayList<>();
+                                            int linenNum = Integer.parseInt(aLocation.getAttribute("line"));
+                                            int numChars = lines.subList(0, linenNum).stream().reduce("", String::concat).length();
+                                            while (m.find() && m.start() < numChars) {
+                                                args.clear();
+                                                for (int k = 0; k < m.groupCount(); ++k) {
+                                                    if (k % 3 == 2 && m.group(k + 1) != null && !m.group(k + 1).equals("")) {
+                                                        args.add(m.group(k + 1));
+                                                        System.out.println("Found arg " + args.get(args.size() - 1));
+                                                    }
                                                 }
                                             }
+                                            int argNum = Integer.parseInt("" + lhs.getTextContent().charAt(lhs.getTextContent().length() - 2));
+                                            if (argNum < args.size()) {
+                                                guess = args.get(argNum - 1);
+                                            } else {
+                                                System.out.println("argNum to big.");
+                                            }
                                         }
-                                        int argNum = Integer.parseInt("" + lhs.getTextContent().charAt(lhs.getTextContent().length() - 2));
-                                        if(argNum < args.size()) {
-                                            guess = args.get(argNum - 1);
-                                        } else {
-                                            System.out.println("argNum to big.");
-                                        }
+                                        references.add(new JBMCOutput.Assignment(aLocation.getAttribute("line"), lhs.getTextContent(), guess, value.getTextContent()));
                                     }
-                                    references.add(new JBMCOutput.Assignment(aLocation.getAttribute("line"), lhs.getTextContent(), guess, value.getTextContent()));
                                 }
                               //  System.out.println("in Line " + aLocation.getAttribute("line") + ": " + lhs.getTextContent() + " = " + value.getTextContent());
                               //  System.out.println(lines.get(Integer.parseInt(aLocation.getAttribute("line")) - 1));
@@ -153,6 +165,7 @@ public class XmlParser {
                         }
                     }
                 }
+                references = checkDynamicObjectAssignments(dynamicObjectsMap, references);
                 if(lineNumber < 0) {
                     res.addProperty(propertyElemnt.getAttribute("property"), references, lineNumber, null);
                 } else {
@@ -160,8 +173,31 @@ public class XmlParser {
                 }
             }
         }
-        if (!someFail) System.out.println("No fail.");
 
+        return res;
+    }
+
+    public static List<JBMCOutput.Assignment> checkDynamicObjectAssignments(Map<String, String> dynObjectsMap, List <JBMCOutput.Assignment> references) {
+        List<JBMCOutput.Assignment> res = new ArrayList<>();
+        if(references == null) {
+            return null;
+        }
+        for(JBMCOutput.Assignment a : references) {
+            if (a.jbmcVarname.startsWith("dynamic_object")) {
+                String dynObject = "";
+                if (a.jbmcVarname.contains(".")) {
+                    dynObject = a.jbmcVarname.substring(0, a.jbmcVarname.indexOf('.'));
+                } else {
+                    dynObject = a.jbmcVarname;
+                }
+                if (dynObjectsMap.containsKey(dynObject)) {
+                    String guess = a.jbmcVarname.replaceAll("dynamic_object(\\d)*", dynObjectsMap.get(dynObject));
+                    res.add(new JBMCOutput.Assignment(a.lineNumber, a.jbmcVarname, guess, a.value));
+                }
+            } else {
+                res.add(a);
+            }
+        }
         return res;
     }
 }
