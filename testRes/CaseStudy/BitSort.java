4a02c3ca14ef5c4392669b3a2c4dff668af3fc81
Jonas Klamroth <klamroth@fzi.de>
2021-01-22 15:46:22 +0100

    added casestudy bitsort

---
+++ testRes/CaseStudy/BitSort.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+public class BitSort {
+    private static void sort(int[] array, int i, int from, int to) {
+        if(i < 0 || to - from <= 1) {
+            return;
+        }
+
+        int mask = 1 << i;
+        int swapPos = from;
+        //System.out.println("");
+        //System.out.println(mask);
+        for(int j = from; j < to; ++j) {
+            //System.out.println(array[j]);
+            //System.out.println(array[j] & mask);
+            //System.out.println(array[1+j] & mask);
+            if((array[j] & mask) == 0 ) {
+                //System.out.println(Arrays.toString(array));
+                swap(array, j, swapPos++);
+                //System.out.println(Arrays.toString(array));
+            }
+        }
+        BitSort.sort(array, i - 1, from, swapPos);
+        BitSort.sort(array, i - 1, swapPos, to);
+    }
+
+    private static void sort(int[] array) {
+        BitSort.sort(array, 30, 0, array.length);
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        //int[] a = new int[]{5, 3, 7, 2};
+        int[] a = new int[]{209823, 1239124, 219281, 234124, 1231, 101239128, 213, 4, 2};
+        BitSort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
