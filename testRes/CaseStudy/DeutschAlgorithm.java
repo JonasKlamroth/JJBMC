3a54877de9ed1b524c2d2e6f1053e1dc6a27f8c9
Jonas Klamroth <klamroth@fzi.de>
2021-03-02 12:04:00 +0100

    added deutsch cs

---
+++ testRes/CaseStudy/DeutschAlgorithm.java
@@ -0,0 +1,66 @@
+public class DeutschAlgorithm {
+    public static void main(String[] args) {
+        Function f = new Function() {
+            @java.lang.Override
+            public boolean exec(boolean input) {
+                return true;
+            }
+        };
+        System.out.println(DeutschAlgorithm.isBalanced(f));
+    }
+
+    //@ requires f != null;
+    //@ ensures \result == (f.exec(false) != f.exec(true));
+    public static boolean isBalanced(Function f) {
+        double h = 0.707;
+        double[] q1 = new double[]{1.0, 0.0};
+        double[] q2 = new double[]{h, -h};
+
+        q1 = new double[]{1*h + 0*h, 1*h + 0*(-h)};
+
+        double[] c = new double[]{q1[0]*q2[0], q1[0]*q2[1], q1[1]*q2[0], q1[1]*q2[1]};
+
+        double [][] m = getOperatorForFunction(f);
+
+        c = new double[]{
+                m[0][0]*c[0] + m[0][1]*c[1] + m[0][2]*c[2] + m[0][3]*c[3],
+                m[1][0]*c[0] + m[1][1]*c[1] + m[1][2]*c[2] + m[1][3]*c[3],
+                m[2][0]*c[0] + m[2][1]*c[1] + m[2][2]*c[2] + m[2][3]*c[3],
+                m[3][0]*c[0] + m[3][1]*c[1] + m[3][2]*c[2] + m[3][3]*c[3]
+        };
+
+        double[][] hm = new double[][]{
+                new double[]{h, 0.f, h, 0.f},
+                new double[]{0.f, h, 0.f, h},
+                new double[]{h, 0.f, -h, 0.f},
+                new double[]{0.f, h, 0.f, -h}
+        };
+
+        c = new double[] {
+                hm[0][0]*c[0] + hm[0][1]*c[1] + hm[0][2]*c[2] + hm[0][3]*c[3],
+                hm[1][0]*c[0] + hm[1][1]*c[1] + hm[1][2]*c[2] + hm[1][3]*c[3],
+                hm[2][0]*c[0] + hm[2][1]*c[1] + hm[2][2]*c[2] + hm[2][3]*c[3],
+                hm[3][0]*c[0] + hm[3][1]*c[1] + hm[3][2]*c[2] + hm[3][3]*c[3]
+        };
+
+        return c[0]*c[0] + c[1]*c[1] < c[2]*c[2] + c[3]*c[3];
+    }
+
+    //@ requires f != null;
+    private static double[][] getOperatorForFunction(Function f) {
+        return new double[][] {
+                new double[]{boolToDouble(!f.exec(false)), boolToDouble(f.exec(false)), 0.f, 0.f},
+                new double[]{boolToDouble(f.exec(false)), boolToDouble(!f.exec(false)), 0.f, 0.f},
+                new double[]{0.f, 0.f, boolToDouble(!f.exec(true)), boolToDouble(f.exec(true))},
+                new double[]{0.f, 0.f, boolToDouble(f.exec(true)), boolToDouble(!f.exec(true))}
+        };
+    }
+
+    private static double boolToDouble(boolean b) {
+        return b ? 1.0 : 0.0;
+    }
+
+    static abstract class Function {
+        public /*@ pure */ abstract boolean exec(boolean input);
+    }
+}
