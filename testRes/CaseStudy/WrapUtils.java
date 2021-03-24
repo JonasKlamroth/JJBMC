4acb5ecfb4479a97f21c2d4a01c1d068314c9194
Jonas Klamroth <klamroth@fzi.de>
2021-03-24 12:35:12 +0100

    simple version of wrap utils

--- testRes/CaseStudy/WrapUtils.java
+++ testRes/CaseStudy/WrapUtils.java
@@ -1,147 +1,70 @@
 public class WrapUtils {
 
-
-    /**
-     * Given a non-null array s and a positive length, this
-     * method replaces ' ' by '\n' characters such that the
-     * length of each line never exceeps length characters.
-     * However, it makes lines as long as possible.
-     *
-     * This method never throws an exception and always
-     * terminates.
-     */
-    /*@ requires s != null && length > 0;
-      @ ensures (\forall int i; i >= 0 && i < s.length; \old(s[i]) == s[i] || (\old(s[i]) == ' ' && s[i] == '\n'));
-      @ ensures (\forall int i; 0 <= i && i < s.length;
-      @             ((\forall int j; i <= j && j < i + length; j < s.length ==>  s[j] != '\n')
-      @         ==> (\forall int j; i <= j && j < i + length; j < s.length ==> s[j] != ' ')));
-      @ ensures (\forall int i; 0 <= i < s.length;
-      @     (\forall int j; i < j < s.length;
-      @     (\forall int k; j < k < s.length;
-      @         (s[i] == '\n' && s[j] == '\n' && \old(s[j]) == ' ' && \old(s[k]) == ' ') ==> k - i >= length)));
+    // Challenge Version, 2nd question.
+    
+    /*@ public normal_behaviour
+      @  requires s != null && lineLength > 0;
       @
-     */
-    public static void wrapLines(char[] s, int length) {
+      @  requires (\forall int i; 0 <= i && i < s.length; s[i] != '\n');
+      @
+      @  ensures (\forall int i; 0 <= i && i < s.length;
+      @    s[i] == \old(s[i]) || \old(s[i]) == ' ' && s[i] == '\n');
+      @
+      @  ensures (\forall int a; 0 <= a < s.length;
+      @     (\forall int b; a < b < s.length;
+      @         s[a] == '\n' && s[b] == '\n' ==> b-a >= lineLength));
+      @
+      @  assignable s[*];
+      @*/    
+    public static void wrapLines(char[] s, int lineLength) {
 
-        int lastChange = -1;
-        int lastSpace = -1;
+        int lastBreak = -1;
+        int lastSpace = indexOf(s, ' ', 0);
 
-
-
-        /*@ loop_invariant s != null;
-          @ loop_invariant -1 <= lastSpace && lastSpace < s.length;
-          @ loop_invariant -1 <= lastChange && lastChange <= lastSpace;
-          @ //loop_invariant lastSpace >= 0 ==> (\old(s)[lastSpace] == ' ' || \old(s)[lastSpace] == '\n');
-          @ loop_invariant (\forall int i; 0 <= i && i < s.length;
-          @    s[i] == \old(s[i]) || (\old(s[i]) == ' ' && s[i] == '\n'));
-          @ loop_invariant lastSpace - lastChange < length ||
-          @    (\forall int l; lastChange < l && l < lastSpace; s[l] != ' ');
-          @ loop_invariant (\forall int i; 0 <= i && i < lastChange; (i < s.length - length) ==>
-          @             ((\forall int j; i <= j && j < i + length; s[j] != '\n')
-          @         ==> (\forall int j; i <= j && j < i + length; s[j] != ' ')));
-          @ loop_invariant lastChange == -1 || s[lastChange] == '\n';
-          @ loop_invariant (\forall int i; 0 <= i <= lastChange;
-          @     (\forall int j; i < j <= lastChange;
-          @     (\forall int k; j < k <= lastChange;
-          @         (s[i] == '\n' && s[j] == '\n' && \old(s[j]) == ' ' && \old(s[k]) == ' ') ==> k - i >= length)));
-          @ decreases s.length - lastSpace;
-          @ loop_modifies s[*], lastSpace, lastChange;
+        /*@
+          @ loop_invariant lastBreak >= -1 && lastBreak < s.length;
+          @ loop_invariant -1 <= lastSpace < s.length;
+          @ loop_invariant (\forall int i; 0 <= i < s.length;
+          @    s[i] == \old(s[i]) || \old(s[i]) == ' ' && s[i] == '\n');
+          @ loop_invariant (\forall int a; 0 <= a < s.length;
+          @     (\forall int b; a < b < s.length;
+          @         s[a] == '\n' && s[b] == '\n' ==> b-a >= lineLength));
+          @ loop_invariant (\forall int a; lastBreak < a && a < s.length;
+          @    s[a] != '\n');
+          @ loop_invariant lastSpace != -1 ==> s[lastSpace] == ' ';
+          @ decreases lastSpace == -1 ? 0 : s.length + 1 - lastSpace;
+          @ loop_modifies s[*], lastBreak, lastSpace;
           @*/
-        while(true) {
-            int nextSpace = indexOf(s, ' ', lastSpace + 1);
-            if(nextSpace == -1) {
-                if(s.length - lastChange >= length && lastSpace >= 0) {
-                    s[lastSpace] = '\n';
-                }
-                return;
+        while(lastSpace != -1) {
+            if(lastSpace - lastBreak > lineLength) {
+                s[lastSpace] = '\n';
+                lastBreak = lastSpace;
             }
-            int nextNewLine = indexOf(s, '\n', lastSpace + 1);
-            if(0 <= nextNewLine && nextNewLine < nextSpace) {
-                if(nextNewLine - lastChange >= length && lastSpace >= 0) {
-                    s[lastSpace] = '\n';
-                }
-                lastChange = lastSpace = nextNewLine;
-            } else {
-                if(nextSpace - lastChange >= length) {
-                    if(lastChange == lastSpace) {
-                        s[nextSpace] = '\n';
-                        lastChange = lastSpace = nextSpace;
-                    } else {
-                        s[lastSpace] = '\n';
-                        lastChange = lastSpace;
-                    }
-                }
-                lastSpace = nextSpace;
-            }
+            lastSpace = indexOf(s, ' ', lastSpace+1);
         }
     }
 
-    /*@ requires s != null && from >= 0;
-      @ ensures (\result == -1 && (\forall int i; i >= from && i < s.length; s[i] != c)) || (\result >= from && \result < s.length && s[\result] == c);
-      @ ensures (\forall int i; i >= from && i < \result; s[i] != c);
-    */
-    private static int indexOf(char[] s, char c, int from) {
+    /*@ private normal_behaviour
+      @  requires s != null && 0 <= from && from <= s.length;
+      @  ensures -1 == \result || from <= \result && \result < s.length;
+      @  ensures \result == -1 ==> 
+      @     (\forall int i; from <= i && i < s.length; s[i] != c);
+      @  ensures \result >= 0 ==>
+      @     s[\result] == c && 
+      @     (\forall int i; from <= i && i < \result; s[i] != c);
+      @  assignable \nothing;
+      @*/
+    private static /*@ helper */ int indexOf(char[] s, char c, int from) {
+        /*@ loop_invariant from <= k && k <= s.length;
+          @ loop_invariant (\forall int i; from <= i && i < k; s[i] != c);
+          @ decreases s.length - k;
+          @ loop_modifies \nothing;
+          @*/
         for(int k = from; k < s.length; k++) {
             if(s[k] == c) {
                 return k;
             }
         }
         return -1;
-    }
-
-    private static int indexBefore(char[] s, char c, int before) {
-        for(int k = before; k > 0; k--) {
-            if(s[k] == c) {
-                return k;
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        char[] a = "\n   \n".toCharArray();
-        WrapUtils.wrapLines(a, 4);
-        System.out.println(a);
-        char[] a1 = "\n   \n".toCharArray();
-        WrapUtils.wrapLines2(a1, 4);
-        System.out.println(a1);
-    }
-
-    /*@ requires s != null && length > 0;
-      @ ensures (\forall int i; i >= 0 && i < s.length; \old(s[i]) == s[i] || (\old(s[i]) == ' ' && s[i] == '\n'));
-      @ ensures (\forall int i; -1 <= i && i < s.length; ((i == -1 || s[i] == '\n')  && i < s.length - length) ==>
-      @             ((\forall int j; i < j && j <= i + length; j <= s.length ==>  s[j] != '\n')
-      @         ==> (\forall int j; i < j && j <= i + length; j <= s.length ==> s[j] != ' ')));
-      @ ensures (\forall int i; 0 <= i < s.length;
-      @     (\forall int j; i < j < s.length;
-      @     (\forall int k; j < k < s.length;
-      @         (s[i] == '\n' && s[j] == '\n' && \old(s[j]) == ' ' && \old(s[k]) == ' ') ==> k - i >= length)));
-      @*/
-    public static void wrapLines2(char[] s, int length) {
-        int lastBreak = -1;
-        int nextBreak = lastBreak + length;
-        int origBreak = indexOf(s, '\n', 0);
-        while(nextBreak < s.length) {
-            if(origBreak > lastBreak && origBreak <= nextBreak) {
-                lastBreak = origBreak;
-                nextBreak = lastBreak + length;
-                origBreak = indexOf(s, '\n', lastBreak + 1);
-            } else {
-                int potBreak = indexBefore(s, ' ', nextBreak);
-                if (potBreak > lastBreak) {
-                    s[potBreak] = '\n';
-                    lastBreak = potBreak;
-                } else {
-                    potBreak = indexOf(s, ' ', lastBreak + 1);
-                    if (potBreak >= 0) {
-                        s[potBreak] = '\n';
-                        lastBreak = potBreak;
-                    } else {
-                        lastBreak = s.length;
-                    }
-                }
-                nextBreak = lastBreak + length;
-            }
-        }
     }
 }
