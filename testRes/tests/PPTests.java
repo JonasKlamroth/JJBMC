8be339ad4cdc9ed00922bf55f2ed568f879fe988
Jonas Klamroth <klamroth@fzi.de>
2023-02-24 17:01:31 +0100

    added file missed in last commit

---
+++ testRes/tests/PPTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import testannotations.Verifyable;
+import testannotations.Fails;
+
+public class PPTests {
+
+    //@ ensures \result == 0;
+    @Verifyable
+    public int inlineOptionTest1() {
+        return inlineOptionTestCallee1(1);
+    }
+
+    //@ requires i > 0;
+    //@ ensures \result == 1;
+    //@ assignable \nothing;
+    @Fails
+    public int inlineOptionTestCallee1(int i) {
+        return i - 1;
+    }
+
+    //@ ensures \result == 0;
+    @Fails
+    public int inlineOptionTest2() {
+        return inlineOptionTestCallee2(1);
+    }
+
+    //@ requires i < 0;
+    //@ ensures \result == 1;
+    //@ assignable \nothing;
+    @Fails
+    public int inlineOptionTestCallee2(int i) {
+        return i - 1;
+    }
+}
